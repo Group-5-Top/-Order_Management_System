19288c93b12316c4b192b55cf46adddb5850e2cc
mikhailwe <mishagorchakov793@gmail.com>
2024-12-24 20:16:53 +0300

    productCommit1

---
+++ src/main/java/ru/tim_5/models/Product.java
@@ -0,0 +1,51 @@
+package ru.tim_5.models;
+
+import java.util.Objects;
+
+public class Product {
+    private final int id;
+    private String name;
+    private double price;
+    private ProductCategory category;
+
+    public Product(int id, String name, double price, ProductCategory category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public ProductCategory getCategory() {
+        return category;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product produck1 = (Product) o;
+        return id == produck1.id && Double.compare(price, produck1.price) == 0 && Objects.equals(name, produck1.name) && category == produck1.category;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price, category);
+    }
+
+    @Override
+    public String toString() {
+        return id + name + '\'' + price + category;
+    }
+}
